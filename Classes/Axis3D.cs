5b15ae6531c6b0f54dc61413d984fbd06944d5e8
mega5800 <43865655+mega5800@users.noreply.github.com>
2021-04-19 19:37:30 +0300

    added moving cubes + OOP approach

---
+++ Classes/Axis3D.cs
@@ -0,0 +1,53 @@
+﻿using OpenGL;
+
+namespace myOpenGL.Classes
+{
+    public class Axis3D
+    {
+        // CLASS MEMBERS
+        private float[] m_FloatArr;
+
+        // CTOR
+        public Axis3D(float[] i_FloatArr = null)
+        {
+            this.m_FloatArr = i_FloatArr;
+        }
+
+        // PUBLIC METHODS
+        public void DrawAxis3D()
+        {
+            if (this.m_FloatArr != null)
+            {
+                GL.glLightfv(GL.GL_LIGHT0, GL.GL_POSITION, this.m_FloatArr);
+                GL.glDisable(GL.GL_LIGHTING);
+                GL.glEnable(GL.GL_LINE_STIPPLE);
+                GL.glLineStipple(1, 0xFF00); // dotted
+            }
+
+            preformAxis3DDrawing();
+            if (this.m_FloatArr != null)
+            {
+                GL.glDisable(GL.GL_LINE_STIPPLE);
+            }
+        }
+
+        // PRIVATE METHODS
+        private void preformAxis3DDrawing()
+        {
+            GL.glBegin(GL.GL_LINES);
+            //x  RED
+            GL.glColor3f(1.0f, 0.0f, 0.0f);
+            GL.glVertex3f(-3.0f, 0.0f, 0.0f);
+            GL.glVertex3f(3.0f, 0.0f, 0.0f);
+            //y  GREEN 
+            GL.glColor3f(0.0f, 1.0f, 0.0f);
+            GL.glVertex3f(0.0f, -3.0f, 0.0f);
+            GL.glVertex3f(0.0f, 3.0f, 0.0f);
+            //z  BLUE
+            GL.glColor3f(0.0f, 0.0f, 1.0f);
+            GL.glVertex3f(0.0f, 0.0f, -3.0f);
+            GL.glVertex3f(0.0f, 0.0f, 3.0f);
+            GL.glEnd();
+        }
+    }
+}
--- GL_Classes/cOGL.cs
+++ GL_Classes/cOGL.cs
@@ -4,101 +4,18 @@ using System.Drawing;
 using myOpenGL.Properties;
 using myOpenGL.Classes;
 using myOpenGL.Structs;
+using System.Collections.Generic;
+using myOpenGL;
 
 namespace OpenGL
 {
     class cOGL
     {
+        #region Original Class Members
         public double hh = 0;
-        Control p;
-        int Width;
-        int Height;
-        GLUquadric obj;
-
-        public cOGL(Control pb)
-        {
-            p = pb;
-            Width = p.Width;
-            Height = p.Height;
-            InitializeGL();
-            obj = GLU.gluNewQuadric(); //do not forget to call gluDeleteQuadric at the end
-        }
-
-        ~cOGL()
-        {
-            GLU.gluDeleteQuadric(obj); //the MUST in case of previous call to gluNewQuadric
-            WGL.wglDeleteContext(m_uint_RC);
-        }
-
-        uint m_uint_HWND = 0;
-
-        public uint HWND
-        {
-            get { return m_uint_HWND; }
-        }
-
-        uint m_uint_DC = 0;
-
-        public uint DC
-        {
-            get { return m_uint_DC; }
-        }
-        uint m_uint_RC = 0;
-
-        public uint RC
-        {
-            get { return m_uint_RC; }
-        }
-
-
-        void DrawOldAxes()
-        {
-            //for this time
-            //Lights positioning is here!!!
-            float[] pos = new float[4];
-            pos[0] = 10; pos[1] = 10; pos[2] = 10; pos[3] = 1;
-            GL.glLightfv(GL.GL_LIGHT0, GL.GL_POSITION, pos);
-            GL.glDisable(GL.GL_LIGHTING);
-
-            //INITIAL axes
-            GL.glEnable(GL.GL_LINE_STIPPLE);
-            GL.glLineStipple(1, 0xFF00);  //  dotted   
-            GL.glBegin(GL.GL_LINES);
-            //x  RED
-            GL.glColor3f(1.0f, 0.0f, 0.0f);
-            GL.glVertex3f(-3.0f, 0.0f, 0.0f);
-            GL.glVertex3f(3.0f, 0.0f, 0.0f);
-            //y  GREEN 
-            GL.glColor3f(0.0f, 1.0f, 0.0f);
-            GL.glVertex3f(0.0f, -3.0f, 0.0f);
-            GL.glVertex3f(0.0f, 3.0f, 0.0f);
-            //z  BLUE
-            GL.glColor3f(0.0f, 0.0f, 1.0f);
-            GL.glVertex3f(0.0f, 0.0f, -3.0f);
-            GL.glVertex3f(0.0f, 0.0f, 3.0f);
-            GL.glEnd();
-            GL.glDisable(GL.GL_LINE_STIPPLE);
-        }
-        void DrawAxes()
-        {
-            GL.glBegin(GL.GL_LINES);
-            //x  RED
-            GL.glColor3f(1.0f, 0.0f, 0.0f);
-            GL.glVertex3f(-3.0f, 0.0f, 0.0f);
-            GL.glVertex3f(3.0f, 0.0f, 0.0f);
-            //y  GREEN 
-            GL.glColor3f(0.0f, 1.0f, 0.0f);
-            GL.glVertex3f(0.0f, -3.0f, 0.0f);
-            GL.glVertex3f(0.0f, 3.0f, 0.0f);
-            //z  BLUE
-            GL.glColor3f(0.0f, 0.0f, 1.0f);
-            GL.glVertex3f(0.0f, 0.0f, -3.0f);
-            GL.glVertex3f(0.0f, 0.0f, 3.0f);
-            GL.glEnd();
-        }
-
-        public bool bChkTexture = false;
-        public int intRadioOption = 1;
+        Control m_ControlInstance;
+        private int m_WidthValue;
+        int m_HeightValue;
 
         public float[] ScrollValue = new float[10];
         public float zShift = 0.0f;
@@ -110,13 +27,178 @@ namespace OpenGL
         public int intOptionC = 0;
         double[] AccumulatedRotationsTraslations = new double[16];
 
+        uint m_uint_HWND = 0;
+        uint m_uint_DC = 0;
+        uint m_uint_RC = 0;
+        #endregion
+
+        #region Original Class Properties
+        public uint HWND
+        {
+            get { return m_uint_HWND; }
+        }
+
+        public uint DC
+        {
+            get { return m_uint_DC; }
+        }
+
+        public uint RC
+        {
+            get { return m_uint_RC; }
+        }
+        #endregion
+
+        #region Original GL Methods
+        protected virtual void InitializeGL()
+        {
+            m_uint_HWND = (uint)m_ControlInstance.Handle.ToInt32();
+            m_uint_DC = WGL.GetDC(m_uint_HWND);
+
+            // Not doing the following WGL.wglSwapBuffers() on the DC will
+            // result in a failure to subsequently create the RC.
+            WGL.wglSwapBuffers(m_uint_DC);
+
+            WGL.PIXELFORMATDESCRIPTOR pfd = new WGL.PIXELFORMATDESCRIPTOR();
+            WGL.ZeroPixelDescriptor(ref pfd);
+            pfd.nVersion = 1;
+            pfd.dwFlags = (WGL.PFD_DRAW_TO_WINDOW | WGL.PFD_SUPPORT_OPENGL | WGL.PFD_DOUBLEBUFFER);
+            pfd.iPixelType = (byte)(WGL.PFD_TYPE_RGBA);
+            pfd.cColorBits = 32;
+            pfd.cDepthBits = 32;
+            pfd.iLayerType = (byte)(WGL.PFD_MAIN_PLANE);
+
+            int pixelFormatIndex = 0;
+            pixelFormatIndex = WGL.ChoosePixelFormat(m_uint_DC, ref pfd);
+            if (pixelFormatIndex == 0)
+            {
+                MessageBox.Show("Unable to retrieve pixel format");
+                return;
+            }
+
+            if (WGL.SetPixelFormat(m_uint_DC, pixelFormatIndex, ref pfd) == 0)
+            {
+                MessageBox.Show("Unable to set pixel format");
+                return;
+            }
+            //Create rendering context
+            m_uint_RC = WGL.wglCreateContext(m_uint_DC);
+            if (m_uint_RC == 0)
+            {
+                MessageBox.Show("Unable to get rendering context");
+                return;
+            }
+            if (WGL.wglMakeCurrent(m_uint_DC, m_uint_RC) == 0)
+            {
+                MessageBox.Show("Unable to make rendering context current");
+                return;
+            }
+
+
+            initRenderingGL();
+        }
+
+        public void OnResize()
+        {
+            m_WidthValue = m_ControlInstance.Width;
+            m_HeightValue = m_ControlInstance.Height;
+            GL.glViewport(0, 0, m_WidthValue, m_HeightValue);
+            Draw();
+        }
+
+        protected virtual void initRenderingGL()
+        {
+            if (m_uint_DC == 0 || m_uint_RC == 0)
+                return;
+            if (this.m_WidthValue == 0 || this.m_HeightValue == 0)
+                return;
+            GL.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+            GL.glEnable(GL.GL_DEPTH_TEST);
+            GL.glDepthFunc(GL.GL_LEQUAL);
+
+            GL.glViewport(0, 0, this.m_WidthValue, this.m_HeightValue);
+            GL.glMatrixMode(GL.GL_PROJECTION);
+            GL.glLoadIdentity();
+
+            //nice 3D
+            GLU.gluPerspective(45.0, 1.0, 0.4, 100.0);
+
+
+            GL.glMatrixMode(GL.GL_MODELVIEW);
+            GL.glLoadIdentity();
+
+            //save the current MODELVIEW Matrix (now it is Identity)
+            GL.glGetDoublev(GL.GL_MODELVIEW_MATRIX, AccumulatedRotationsTraslations);
+
+            //! TEXTURE 1a 
+            generateTextures();
+            //! TEXTURE 1b 
+        }
+
+        private void generateTextures()
+        {
+            GL.glEnable(GL.GL_TEXTURE_2D);
+
+            m_TextureUIntArray = new uint[1];		// storage for texture
+
+            Bitmap image = new Bitmap(Resources.cubeTexture);
+            image.RotateFlip(RotateFlipType.RotateNoneFlipY); //Y axis in Windows is directed downwards, while in OpenGL-upwards
+            System.Drawing.Imaging.BitmapData bitmapdata;
+            Rectangle rect = new Rectangle(0, 0, image.Width, image.Height);
+
+            bitmapdata = image.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadOnly,
+                System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+
+            GL.glGenTextures(1, m_TextureUIntArray);
+            GL.glBindTexture(GL.GL_TEXTURE_2D, m_TextureUIntArray[0]);
+            //  VN-in order to use System.Drawing.Imaging.BitmapData Scan0 I've added overloaded version to
+            //  OpenGL.cs
+            //  [DllImport(GL_DLL, EntryPoint = "glTexImage2D")]
+            //  public static extern void glTexImage2D(uint target, int level, int internalformat, int width, int height, int border, uint format, uint type, IntPtr pixels);
+            GL.glTexImage2D(GL.GL_TEXTURE_2D, 0, (int)GL.GL_RGB8, image.Width, image.Height,
+                0, GL.GL_BGR_EXT, GL.GL_UNSIGNED_byte, bitmapdata.Scan0);
+
+            GL.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MIN_FILTER, (int)GL.GL_LINEAR);		// Linear Filtering
+            GL.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MAG_FILTER, (int)GL.GL_LINEAR);		// Linear Filtering
+
+            image.UnlockBits(bitmapdata);
+            image.Dispose();
+        }
+        #endregion
+
+        private List<SecretBox> m_SecretBoxesList = new List<SecretBox>();
+        private Form1 m_Form1Instance;
+        private uint[] m_TextureUIntArray;
+        private Axis3D m_StaticAxis3D;
+        private Axis3D m_DynamicAxis3D;
+
+        // CTOR
+        public cOGL(Control i_ControlInstance, Form1 i_Form1Instance)
+        {
+            this.m_Form1Instance = i_Form1Instance;
+            m_ControlInstance = i_ControlInstance;
+            m_WidthValue = m_ControlInstance.Width;
+            m_HeightValue = m_ControlInstance.Height;
+            InitializeGL();
+
+            this.m_StaticAxis3D = new Axis3D(new float[] { 10, 10, 10, 1 });
+            this.m_DynamicAxis3D = new Axis3D();
+            this.fillSecretBoxesList(5);
+        }
+
+        // DTOR
+        ~cOGL()
+        {
+            WGL.wglDeleteContext(m_uint_RC);
+        }
+
+        // PUBLIC METHODS
         public void Draw()
         {
             if (m_uint_DC == 0 || m_uint_RC == 0)
                 return;
 
             GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
-
 
             //FULL and COMPLICATED
             // GL.glViewport(0,Height/2,Width,Height/2);						
@@ -132,7 +214,7 @@ namespace OpenGL
                        ScrollValue[6], ScrollValue[7], ScrollValue[8]);
             GL.glTranslatef(0.0f, 0.0f, -4.0f);
 
-            DrawOldAxes();
+            this.m_StaticAxis3D.DrawAxis3D();
 
             //save current ModelView Matrix values
             //in ModelVievMatrixBeforeSpecificTransforms array
@@ -143,9 +225,12 @@ namespace OpenGL
 
             //make transformation in accordance to KeyCode
             float delta;
-            if (intOptionC != 0)
+            if ((intOptionC != 0) && (this.m_Form1Instance.NumericUpDownValueChanged))
             {
+                m_Form1Instance.NumericUpDownValueChanged = false;
                 delta = 5.0f * Math.Abs(intOptionC) / intOptionC; // signed 5
+                Console.WriteLine("delta=" + delta);
+                Console.WriteLine("intOptionC=" + intOptionC);
 
                 switch (Math.Abs(intOptionC))
                 {
@@ -199,17 +284,13 @@ namespace OpenGL
             GL.glMultMatrixd(AccumulatedRotationsTraslations);
 
             GL.glEnable(GL.GL_TEXTURE_2D);
-            GL.glBindTexture(GL.GL_TEXTURE_2D, m_Texture[0]);
-           
-            DrawAxes();
+            GL.glBindTexture(GL.GL_TEXTURE_2D, m_TextureUIntArray[0]);
+
+            this.m_DynamicAxis3D.DrawAxis3D();
             GL.glColor3f(1.0f, 1.0f, 1.0f);
 
             //test
-            SecretBox secretBox = new SecretBox(new Point3D(0.0f, 0.0f, 0.5f));
-            secretBox.DrawSecretBox();
-
-            SecretBox secretBox1 = new SecretBox(new Point3D(1.0f, 2.0f, 1.0f));
-            secretBox1.DrawSecretBox();
+            drawSecretBoxesList();
             //test
 
             GL.glFlush();
@@ -217,208 +298,21 @@ namespace OpenGL
             GL.glDisable(GL.GL_TEXTURE_2D);
         }
 
-        private void drawCubeAndPasteTexture(float i_Height)
+        // PRIVATE METHODS
+        private void fillSecretBoxesList(int i_AmountOfSecretBoxesToCreate)
         {
-            GL.glBegin(GL.GL_QUADS);
-
-            //Back side
-            GL.glTexCoord2f(0.5f, 0.0f);
-            GL.glVertex3f(0.0f, 0.0f + i_Height, 0.0f);
-
-            GL.glTexCoord2f(0.0f, 0.0f);
-            GL.glVertex3f(1.0f, 0.0f + i_Height, 0.0f);
-
-            GL.glTexCoord2f(0.0f, 1.0f);
-            GL.glVertex3f(1.0f, 1.0f + i_Height, 0.0f);
-
-            GL.glTexCoord2f(0.5f, 1.0f);
-            GL.glVertex3f(0.0f, 1.0f + i_Height, 0.0f);
-
-            //Left side
-            GL.glTexCoord2f(0.0f, 0.0f);
-            GL.glVertex3f(0.0f, 0.0f + i_Height, 0.0f);
-
-            GL.glTexCoord2f(0.5f, 0.0f);
-            GL.glVertex3f(0.0f, 0.0f + i_Height, 1.0f);
-
-            GL.glTexCoord2f(0.5f, 1.0f);
-            GL.glVertex3f(0.0f, 1.0f + i_Height, 1.0f);
-
-            GL.glTexCoord2f(0.0f, 1.0f);
-            GL.glVertex3f(0.0f, 1.0f + i_Height, 0.0f);
-
-            //Right side
-            GL.glTexCoord2f(0.5f, 0.0f);
-            GL.glVertex3f(1.0f, 0.0f + i_Height, 0.0f);
-
-            GL.glTexCoord2f(0.0f, 0.0f);
-            GL.glVertex3f(1.0f, 0.0f + i_Height, 1.0f);
-
-            GL.glTexCoord2f(0.0f, 1.0f);
-            GL.glVertex3f(1.0f, 1.0f + i_Height, 1.0f);
-
-            GL.glTexCoord2f(0.5f, 1.0f);
-            GL.glVertex3f(1.0f, 1.0f + i_Height, 0.0f);
-
-            //FrontSide
-            GL.glTexCoord2f(0.0f, 0.0f);
-            GL.glVertex3f(0.0f, 0.0f + i_Height, 1.0f);
-
-            GL.glTexCoord2f(0.5f, 0.0f);
-            GL.glVertex3f(1.0f, 0.0f + i_Height, 1.0f);
-
-            GL.glTexCoord2f(0.5f, 1.0f);
-            GL.glVertex3f(1.0f, 1.0f + i_Height, 1.0f);
-
-            GL.glTexCoord2f(0.0f, 1.0f);
-            GL.glVertex3f(0.0f, 1.0f + i_Height, 1.0f);
-
-            //upper case
-            GL.glTexCoord2f(0.5f, 0.0f);
-            GL.glVertex3f(0.0f, 1.0f + i_Height, 0.0f);
-
-            GL.glTexCoord2f(0.5f, 1.0f);
-            GL.glVertex3f(1.0f, 1.0f + i_Height, 0.0f);
-
-            GL.glTexCoord2f(1.0f, 1.0f);
-            GL.glVertex3f(1.0f, 1.0f + i_Height, 1.0f);
-
-            GL.glTexCoord2f(1.0f, 0.0f);
-            GL.glVertex3f(0.0f, 1.0f + i_Height, 1.0f);
-
-            //bottom case
-            GL.glTexCoord2f(0.5f, 0.0f);
-            GL.glVertex3f(0.0f, 0.0f + i_Height, 0.0f);
-
-            GL.glTexCoord2f(0.5f, 1.0f);
-            GL.glVertex3f(1.0f, 0.0f + i_Height, 0.0f);
-
-            GL.glTexCoord2f(1.0f, 1.0f);
-            GL.glVertex3f(1.0f, 0.0f + i_Height, 1.0f);
-
-            GL.glTexCoord2f(1.0f, 0.0f);
-            GL.glVertex3f(0.0f, 0.0f + i_Height, 1.0f);
-
-            GL.glEnd();
+            for (int i = 0; i < i_AmountOfSecretBoxesToCreate; i++)
+            {
+                this.m_SecretBoxesList.Add(new SecretBox(new Point3D(0.0f + i * 1.5f, 0.0f, 0.5f)));
+            }
         }
 
-        protected virtual void InitializeGL()
+        private void drawSecretBoxesList()
         {
-            m_uint_HWND = (uint)p.Handle.ToInt32();
-            m_uint_DC = WGL.GetDC(m_uint_HWND);
-
-            // Not doing the following WGL.wglSwapBuffers() on the DC will
-            // result in a failure to subsequently create the RC.
-            WGL.wglSwapBuffers(m_uint_DC);
-
-            WGL.PIXELFORMATDESCRIPTOR pfd = new WGL.PIXELFORMATDESCRIPTOR();
-            WGL.ZeroPixelDescriptor(ref pfd);
-            pfd.nVersion = 1;
-            pfd.dwFlags = (WGL.PFD_DRAW_TO_WINDOW | WGL.PFD_SUPPORT_OPENGL | WGL.PFD_DOUBLEBUFFER);
-            pfd.iPixelType = (byte)(WGL.PFD_TYPE_RGBA);
-            pfd.cColorBits = 32;
-            pfd.cDepthBits = 32;
-            pfd.iLayerType = (byte)(WGL.PFD_MAIN_PLANE);
-
-            int pixelFormatIndex = 0;
-            pixelFormatIndex = WGL.ChoosePixelFormat(m_uint_DC, ref pfd);
-            if (pixelFormatIndex == 0)
+            foreach (SecretBox secretBox in this.m_SecretBoxesList)
             {
-                MessageBox.Show("Unable to retrieve pixel format");
-                return;
+                secretBox.DrawSecretBox();
             }
-
-            if (WGL.SetPixelFormat(m_uint_DC, pixelFormatIndex, ref pfd) == 0)
-            {
-                MessageBox.Show("Unable to set pixel format");
-                return;
-            }
-            //Create rendering context
-            m_uint_RC = WGL.wglCreateContext(m_uint_DC);
-            if (m_uint_RC == 0)
-            {
-                MessageBox.Show("Unable to get rendering context");
-                return;
-            }
-            if (WGL.wglMakeCurrent(m_uint_DC, m_uint_RC) == 0)
-            {
-                MessageBox.Show("Unable to make rendering context current");
-                return;
-            }
-
-
-            initRenderingGL();
-        }
-
-        public void OnResize()
-        {
-            Width = p.Width;
-            Height = p.Height;
-            GL.glViewport(0, 0, Width, Height);
-            Draw();
-        }
-
-        protected virtual void initRenderingGL()
-        {
-            if (m_uint_DC == 0 || m_uint_RC == 0)
-                return;
-            if (this.Width == 0 || this.Height == 0)
-                return;
-            GL.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-            GL.glEnable(GL.GL_DEPTH_TEST);
-            GL.glDepthFunc(GL.GL_LEQUAL);
-
-            GL.glViewport(0, 0, this.Width, this.Height);
-            GL.glMatrixMode(GL.GL_PROJECTION);
-            GL.glLoadIdentity();
-
-            //nice 3D
-            GLU.gluPerspective(45.0, 1.0, 0.4, 100.0);
-
-
-            GL.glMatrixMode(GL.GL_MODELVIEW);
-            GL.glLoadIdentity();
-
-            //save the current MODELVIEW Matrix (now it is Identity)
-            GL.glGetDoublev(GL.GL_MODELVIEW_MATRIX, AccumulatedRotationsTraslations);
-
-            //! TEXTURE 1a 
-            GenerateTextures();
-            //! TEXTURE 1b 
-        }
-
-
-        //! TEXTURE b
-        private uint[] m_Texture;
-
-        void GenerateTextures()
-        {
-            GL.glEnable(GL.GL_TEXTURE_2D);
-
-            m_Texture = new uint[1];		// storage for texture
-
-            Bitmap image = new Bitmap(Resources.cubeTexture);
-            image.RotateFlip(RotateFlipType.RotateNoneFlipY); //Y axis in Windows is directed downwards, while in OpenGL-upwards
-            System.Drawing.Imaging.BitmapData bitmapdata;
-            Rectangle rect = new Rectangle(0, 0, image.Width, image.Height);
-
-            bitmapdata = image.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadOnly,
-                System.Drawing.Imaging.PixelFormat.Format24bppRgb);
-
-            GL.glGenTextures(1, m_Texture);
-            GL.glBindTexture(GL.GL_TEXTURE_2D, m_Texture[0]);
-            //  VN-in order to use System.Drawing.Imaging.BitmapData Scan0 I've added overloaded version to
-            //  OpenGL.cs
-            //  [DllImport(GL_DLL, EntryPoint = "glTexImage2D")]
-            //  public static extern void glTexImage2D(uint target, int level, int internalformat, int width, int height, int border, uint format, uint type, IntPtr pixels);
-            GL.glTexImage2D(GL.GL_TEXTURE_2D, 0, (int)GL.GL_RGB8, image.Width, image.Height,
-                0, GL.GL_BGR_EXT, GL.GL_UNSIGNED_byte, bitmapdata.Scan0);
-
-            GL.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MIN_FILTER, (int)GL.GL_LINEAR);		// Linear Filtering
-            GL.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MAG_FILTER, (int)GL.GL_LINEAR);		// Linear Filtering
-
-            image.UnlockBits(bitmapdata);
-            image.Dispose();
         }
     }
 }
