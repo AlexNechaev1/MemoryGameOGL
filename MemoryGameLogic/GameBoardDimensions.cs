22f19c6b3f01d9c81747f42534f50f26da92eff0
mega5800 <43865655+mega5800@users.noreply.github.com>
2021-05-01 00:57:58 +0300

    added logic project

---
+++ MemoryGameLogic/GameBoardDimensions.cs
@@ -0,0 +1,35 @@
+﻿namespace MemoryGameLogic
+{
+    public struct GameBoardDimensions
+    {
+        // MEMBER VARIABLES
+        private readonly int r_Height;
+        private readonly int r_Width;
+
+        // CTOR
+        public GameBoardDimensions(int i_Height, int i_Width)
+        {
+            this.r_Height = i_Height;
+            this.r_Width = i_Width;
+        }
+
+        // PROPERTIES
+        public int Height
+        {
+            get { return this.r_Height; }
+        }
+
+        public int Width
+        {
+            get { return this.r_Width; }
+        }
+
+        // TOSTRING METHOD
+        public override string ToString()
+        {
+            string stringToReturn = string.Format("{0} x {1}", this.r_Height, this.r_Width);
+
+            return stringToReturn;
+        }
+    }
+}
---
+++ MemoryGameLogic/MatrixIndex.cs
@@ -0,0 +1,27 @@
+﻿namespace MemoryGameLogic
+{
+    public struct MatrixIndex
+    {
+        // MEMBER VARIABLES
+        private readonly int r_MatrixRowIndex;
+        private readonly int r_MatrixColumnIndex;
+
+        // CTOR
+        public MatrixIndex(int i_RowIndex, int i_ColumnIndex)
+        {
+            this.r_MatrixRowIndex = i_RowIndex;
+            this.r_MatrixColumnIndex = i_ColumnIndex;
+        }
+
+        // PROPERTIES
+        public int MatrixRowIndex
+        {
+            get { return this.r_MatrixRowIndex; }
+        }
+
+        public int MatrixColumnIndex
+        {
+            get { return this.r_MatrixColumnIndex; }
+        }
+    }
+}
